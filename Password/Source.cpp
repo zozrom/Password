89f409b11ad336474692fd07e053cf7d735d559c
zozrom <zozulia.roman135@gmail.com>
2018-12-25 13:57:50 +0200

    Second commit

--- Password/Source.cpp
+++ Password/Source.cpp
@@ -2,8 +2,39 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
+bool password();
 int main()
-{  
+{
+	if (password())
+	{
+
+	}
 	// Password : programming 
 	return 0; 
+}
+bool password()
+{
+	for (int  i = 0; i < 3; i++)
+	{
+		int x,v=3;
+		char array[50];
+		do
+		{
+			cout << "You have " << v << " attempt" << " enter the password :";
+			gets_s(array);
+			strlen(array);//Numbers of digitals
+			x = strlen(array);
+		} while (0>x || x>=49);
+		if (!strcmp(array,"programming"))
+		{
+			cout << "Password enter corectly," << " ";
+			return true;
+		}
+		else
+		{
+			cout << "Invalid password!";
+			cout << endl;
+		}
+	}
+	return false;
 }
